991362f0789edf63bc26a4fcf8fbf683115a3354
pd <pd.shahsafi@gmail.com>
2017-04-17 17:59:04 +0430

    final test

--- filter.py
+++ filter.py
@@ -1,1 +1,8 @@
-print("helloworld")
+from skimage import data, io, filters
+
+image = data.coins()
+# ... or any other NumPy array!
+edges = filters.sobel(image)
+io.imshow(edges)
+io.show()
+print(image)
